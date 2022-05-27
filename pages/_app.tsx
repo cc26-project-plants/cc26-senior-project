dcf4e3a93b43e67d5552aeadbad2adbf900c7f09
stk-m <stk.murakami@gmail.com>
2022-05-27 17:16:14 +0900

    feat: add Navbar.tsx and install tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
+import 'tailwindcss/tailwind.css';
 import "../src/config/firebase.config";
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "../src/hook/auth";
 import AuthStateChanged from "../src/layout/AuthStateChanged";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
