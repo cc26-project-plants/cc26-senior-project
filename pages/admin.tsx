d9d3301a034ebc9d7b2b89d70d2bceba6f271180
lzs95 <86816779+lzs95@users.noreply.github.com>
2022-05-30 10:34:45 +0900

    fix: admin and main page update

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { withProtected } from "../src/hook/route";
+import { Button } from "react-bootstrap";
+import Link from "next/link";
 
+//className;
+//line 13: tailwind
+//button: bootstrap
 function Admin({ auth }: { auth: any }) {
   const { logout } = auth;
   return (
     <div>
-      <button onClick={logout}>Logout</button>
+      <div className="flex flex-col container mx-auto p-8">
+        <Link href="/main">
+          <Button type="button" className="btn btn-info w-25 my-5">
+            Go Back to Main Page
+          </Button>
+        </Link>
+        <Button
+          type="button"
+          className="btn btn-success w-25 my-5"
+          onClick={logout}
+        >
+          Log out
+        </Button>
+      </div>
     </div>
   );
 }
