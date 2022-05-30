3bed95d824a4ffdef4202922b8ef8cd32277d774
stk-m <stk.murakami@gmail.com>
2022-05-30 16:16:10 +0900

    feat: add charts and Data.js

---
+++ pages/charts/LightLevel.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  CategoryScale,
+  BarElement,
+} from "chart.js";
+
+function LightLevel({ chartData }) {
+  ChartJS.register(
+    LineController,
+    LineElement,
+    PointElement,
+    LinearScale,
+    Title,
+    CategoryScale,
+    BarElement
+  );
+  return <Line data={chartData} />;
+}
+export default LightLevel
--- pages/main.tsx
+++ pages/main.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useDebugValue, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Link from "next/link";
-import { withProtected } from "../src/hook/route";
+import { PlantData } from "./charts/Data";
+// import Link from "next/link";
+// import { withProtected } from "../src/hook/route";
 
 import Navbar from "./Navbar ";
+import LightLevel from "./charts/lightlevel";
 
 const Main = () => {
   // const { user, loginWithGoogle, error }: any = auth;
+  const [tempData, setTempData] = useState({
+    labels: PlantData.map((val) => val.id),
+    datasets: [
+      {
+        labels: "Temp",
+        data: PlantData.map((val) => val.temperature),
+      },
+    ],
+  });
   return (
     <div>
       <div className="font-mono max-w-screen h-14 bg-green-100 align-middle ">
@@ -16,6 +27,9 @@ const Main = () => {
         <Navbar />
         <div className="font-mono  w-screen h-screen  bg-green-600">
           Main Field here
+          <div>
+            <LightLevel chartData={tempData} />
+          </div>
           {/* {modalShow && <FeedPlant onClick={displayModal}/>} */}
           {/* {modalShow && <FeedPlant onClick={displayModal}/>} */}
         </div>
