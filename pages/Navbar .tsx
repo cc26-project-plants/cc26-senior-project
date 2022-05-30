2df4391d887bd43b1dba3defe2e1a9a392ca9bd5
stk-m <stk.murakami@gmail.com>
2022-05-30 13:45:17 +0900

    feat: fix components

--- pages/Navbar .tsx
+++ pages/Navbar .tsx
@@ -7,17 +7,35 @@ import { useAuth } from "../context/AuthContext"
 export default function Navbar({}){
     const { logout } = useAuth();
     const menu: { name: string; link: string}[]=[
-        {name:"Feed my plant", link:"/"},
-        {name:"Check Camera", link:"/"},
-        {name:"Check Humidity", link:"/"},
-        {name:"Check Soil moist", link:"/"},
-        {name:"Check Temperature", link:"/"},
+        // {name:"Feed my plant", link:"/"},
+        {name:"Check Camera", link:"/CheckCamera"},
+        {name:"Check Humidity", link:"/Humidity"},
+        {name:"Check Soil moist", link:"/SoilMoist"},
+        {name:"Check Temperature", link:"/Temperature"},
     ]
-    let [open, setOpen] = useState(false);
+    // let [open, setOpen] = useState(false);
+        // const [open, setOpen] = useState(false)
+    // const toggle = ()=>{
+    //     setOpen(prevState => !prevState)
+    // }
+    const [modalShow, setModalShow]= useState(false)
+    const displayModal = ()=>{
+      setModalShow(true)
+    }
 
     return (
     <div className="h-96 w-48 bg-lime-400 list-none">
         <div>
+        <li>
+            <div
+            onClick={displayModal}
+            // href={"/"} 
+            className="font-mono w-40 inline-block text-sm px-4 py-2 leading-none border rounded
+            no-underline text-green-700 border-white 
+            hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            >Feed my plant
+            </div>
+        </li>
         {menu.map((menu) => (
             <li key={menu.name}>
                 <a 
@@ -33,7 +51,7 @@ export default function Navbar({}){
         </div>
         <Link href="/admin">
             <a
-            className="w-40 inline-block text-sm px-4 py-2 leading-none border rounded
+            className="font-mono w-40 inline-block text-sm px-4 py-2 leading-none border rounded
             no-underline text-green-700 border-white 
             hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 mb-8 lg:mt-0"
             >Logout</a>
