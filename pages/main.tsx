d379fc812e089de2a5b6f52f8d192e63d9fec3dd
lzs95 <86816779+lzs95@users.noreply.github.com>
2022-05-31 15:17:19 +0900

    feat:  delete files, added button, working on data

--- pages/main.tsx
+++ pages/main.tsx
@@ -1,24 +1,71 @@
 import React, { useDebugValue, useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import PlantData from "./charts/Data.json";
+import PlantData from "./charts/Data.json";
 import Link from "next/link";
 import { withProtected } from "../src/hook/route";
 import Navbar from "./Navbar ";
 import TempLevel from "./charts/TempLevel";
+import LightLevel from "./charts/LightLevel";
+
+interface PlantData {
+  Data: any;
+}
 
 const Main = () => {
-  // const [tempData, setTempData] = useState({
-  //   labels: PlantData[0].data.map((val) => Date.parse(val.createdAt).toFixed()),
-  //   datasets: [
-  //     {
-  //       label: "Temperature",
-  //       data: PlantData[0].data.map((val) => val.temperature),
-  //       backgroundColor: ["rgba(75,192,192,1)"],
-  //       borderColor: "black",
-  //       borderWidth: 2,
-  //     },
-  //   ],
-  // });
+  const [showLight, setShowLight] = useState(false);
+  const [showTemp, setShowTemp] = useState(false);
+  const [showSoilWater, setSoilWater] = useState(false);
+  const hideEverything = () => {
+    setShowLight(false);
+    setShowTemp(false);
+    setSoilWater(false);
+  };
+  const displayLight = () => {
+    hideEverything();
+    setShowLight(true);
+  };
+  const displayTemp = () => {
+    hideEverything();
+    setShowTemp(true);
+  };
+  const displaySoilWater = () => {
+    setShowLight(false);
+    setShowTemp(false);
+    setSoilWater(true);
+  };
+  let plantTemperatureArray: string[] = [];
+  let plantTemperatureTimeArray: number[] = [];
+  function getTemp() {
+    let plantDataValues =
+      PlantData.data._fieldsProto.sensorData.arrayValue.values;
+
+    //Return Data
+    plantTemperatureArray = plantDataValues.map(
+      (val: any) => val.mapValue.fields.temperature.integerValue
+    );
+    plantTemperatureTimeArray = plantDataValues.map(
+      (val: any) => val.mapValue.fields.timestamp.timestampValue.nanos
+    );
+    console.log(plantTemperatureArray);
+    console.log(plantTemperatureTimeArray);
+  }
+
+  const [tempData, setTempData] = useState({
+    labels: plantTemperatureTimeArray,
+    datasets: [
+      {
+        label: "Temperature",
+        data: plantTemperatureArray,
+        backgroundColor: ["rgba(75,192,192,1)"],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    getTemp();
+  }, []);
 
   return (
     <div>
@@ -27,13 +74,38 @@ const Main = () => {
       </div>
       <div className="flex flex-row  ">
         <Navbar />
-        <div className="font-mono  w-screen h-screen ">
-          Main Field here
-          <div className=" w-1/2 ">
-            {/* <TempLevel chartData={tempData} /> */}
+        <div className="font-mono  w-screen h-screen  bg-green-600">
+          <div>
+            <button
+              onClick={displayLight}
+              className="w-40 inline-block text-sm px-4 py-2 leading-none border rounded
+            no-underline text-yellow-100 border-white 
+            hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            >
+              light
+            </button>
+
+            <button
+              onClick={displayTemp}
+              className="w-40 inline-block text-sm px-4 py-2 leading-none border rounded
+            no-underline text-yellow-100 border-white 
+            hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            >
+              Temperature
+            </button>
+
+            <button
+              // onClick={displayTemp}
+              className="w-40 inline-block text-sm px-4 py-2 leading-none border rounded
+            no-underline text-yellow-100 border-white 
+            hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            >
+              Soil Moisture
+            </button>
+
+            {showLight && <LightLevel chartData={tempData} />}
+            {showTemp && <TempLevel chartData={tempData} />}
           </div>
-          {/* {modalShow && <FeedPlant onClick={displayModal}/>} */}
-          {/* {modalShow && <FeedPlant onClick={displayModal}/>} */}
         </div>
       </div>
     </div>
@@ -41,43 +113,3 @@ const Main = () => {
 };
 
 export default Main;
-
-// const getLightData = () => {
-//   let arr: number[] = [];
-//   let labels: string[] = [];
-//   let datasets: { label: string; data: any }[] = [];
-//   PlantData[0].data.map((val) => {
-//     labels.push(val.createdAt);
-//     datasets.push({
-//       label: "Light",
-//       data: arr.push(val.lightLevel),
-//     });
-//   });
-//   console.log(datasets);
-//   console.log(arr);
-//   setTempData({ labels, datasets });
-// };
-
-// useEffect(() => {
-//   return getLightData();
-// }, []);
-
-// const getLightData = () => {
-//   let labels: string[] = [];
-//   let datasets: { label: string; data: number[] }[] = [
-//     {
-//       label: "Temperature",
-//       data: [],
-//     },
-//   ];
-//   PlantData[0].data.map((val) => {
-//     labels.push(val.createdAt);
-//     datasets[0].data.push(val.temperature);
-//   });
-
-//   setTempData({ labels, datasets });
-// };
-
-// useEffect(() => {
-//   getLightData();
-// }, []);
