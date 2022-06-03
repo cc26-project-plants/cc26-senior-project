927839146955bc11f4b25252cd5117a6dbb30f3f
lzs95 <86816779+lzs95@users.noreply.github.com>
2022-06-03 16:06:00 +0900

    style: working on welcome page

--- pages/components/WelcomeScreen.tsx
+++ pages/components/WelcomeScreen.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import next from "next";
 
-export default function WelcomePage() {
+export default function WelcomeScreen() {
   return (
-    <div className="z-50  absolute  flex justify-center  items-center bg-gray-300  shadow-lg overscroll-contain overflow-auto">
+    <div className="w-screen  flex justify-center  items-center bg-gray-300  shadow-lg overscroll-contain overflow-auto">
       <div className=" text-center">WelcomePage</div>
     </div>
   );
--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import TopBar from "./components/TopBar";
+import WelcomeScreen from "./components/WelcomeScreen";
+
 export default function welcome() {
   return (
     <div className="flex flex-col h-screen ">
       <TopBar />
-      <Navbar />
+      <div className="flex flex-row h-full">
+        <Navbar />
+        <WelcomeScreen />
+      </div>
     </div>
   );
 }
