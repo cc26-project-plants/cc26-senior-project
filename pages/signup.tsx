813fbd12f40b040530225c823d12be31fe45570e
lzs95 <86816779+lzs95@users.noreply.github.com>
2022-06-06 19:14:33 +0900

    style: signup form re-stylized

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -52,10 +52,139 @@ const Signup = () => {
   };
 
   return (
-    <div className="bg-loginBg h-screen w-screen flex ">
+    <div>
+      <div className="bg-loginBg h-screen w-screen flex items-center text-white">
+        <div className="w-1/6"></div>
+        <div className="bg-gray-400 bg-opacity-50 p-10 rounded-md outline outline-white w-1/3 min-w-fit min-h-fit">
+          <h2 className="text-start font-thin text-6xl">Signup</h2>
+          <form onSubmit={handleSignup} className="grid grid-cols-2 gap-4">
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label className="font-semibold mt-6">User Name</Form.Label>
+              <Form.Control
+                type="userName"
+                placeholder="Your name"
+                required
+                onChange={(e: any) =>
+                  setData({
+                    ...data,
+                    userName: e.target.value,
+                  })
+                }
+                value={data.userName}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail" className=" mt-2">
+              <Form.Label className="font-semibold -mb-2">
+                Plant Name
+              </Form.Label>
+              <p>Please give your plant a nickname</p>
+              <Form.Control
+                placeholder="Plant name"
+                required
+                onChange={(e: any) =>
+                  setData({
+                    ...data,
+                    plantName: e.target.value,
+                  })
+                }
+                value={data.plantName}
+              />
+            </Form.Group>
+
+            <Form.Group className="mt-2" controlId="formBasicEmail">
+              <Form.Label className="font-semibold">Plant Type</Form.Label>
+              <Form.Select
+                required
+                onChange={(e: any) =>
+                  setData({
+                    ...data,
+                    plantType: e.target.value,
+                  })
+                }
+                value={data.plantType}
+              >
+                <option>Choose...</option>
+                <option value="Cactus（サボテン）">Cactus（サボテン）</option>
+                <option value="Sword Leaf（ソードリーフ">
+                  Sword Leaf（ソードリーフ）
+                </option>
+                <option value="Benjamin（ベンジャミン）">
+                  Benjamin（ベンジャミン）
+                </option>
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail" className="-mt-2">
+              <Form.Label className="font-semibold -mb-2">Plant ID</Form.Label>
+              <p>Please check the Plant ID enclosed with the device</p>
+              <Form.Control
+                type="text"
+                placeholder="Plant ID"
+                required
+                onChange={(e: any) =>
+                  setData({
+                    ...data,
+                    plantID: e.target.value,
+                  })
+                }
+                value={data.plantID}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label className="font-semibold">Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                required
+                onChange={(e: any) =>
+                  setData({
+                    ...data,
+                    email: e.target.value,
+                  })
+                }
+                value={data.email}
+              />
+            </Form.Group>
+
+            <Form.Group className="-mt-4" controlId="formBasicPassword">
+              <Form.Label className="font-semibold -mb-1">Password</Form.Label>
+              <p>* more than 6 characters with 2 numbers</p>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                required
+                onChange={(e: any) =>
+                  setData({
+                    ...data,
+                    _password: e.target.value,
+                  })
+                }
+                value={data._password}
+              />
+            </Form.Group>
+            <button
+              // variant="primary"
+              type="submit"
+              className=" text-white min-w-1/2 w-100 bg-teal-600 outline outline-1 h-16 mb-9  rounded-md outline-white mt-2 hover:text-white hover:bg-teal-400"
+            >
+              Signup
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
+
+{
+  /* <div className="bg-loginBg h-screen w-screen flex items-start">
       <div className="w-1/6"></div>
-      <div className="bg-gray-400 bg-opacity-50 flex pl-12 pr-12 pt-2 rounded-md mt-3 outline outline-white min-w-fit min-h-fit w-fit h-fit">
-        <form onSubmit={handleSignup} className="text-white flex flex-col">
+      <div className="bg-gray-400 bg-opacity-50 flex pl-12 pr-12 pt-2 rounded-md mt-6 outline outline-white min-w-fit min-h-fit w-fit h-fit ">
+        <form onSubmit={handleSignup} className="text-white flex flex-col ">
           <h2 className="text-center font-thin">Signup</h2>
 
           <Form.Group controlId="formBasicEmail">
@@ -172,8 +301,5 @@ const Signup = () => {
           </button>
         </form>
       </div>
-    </div>
-  );
-};
-
-export default Signup;
+    </div> */
+}
