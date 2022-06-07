2d8f8b3c7cb48cf930935bdf5d575e8dd0ba50fb
stk-m <stk.murakami@gmail.com>
2022-06-07 19:56:16 +0900

    feat: insert mqtt.connect in ControlPanel.tsx

--- pages/components/ControlPanel.tsx
+++ pages/components/ControlPanel.tsx
@@ -1,21 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { Form } from "react-bootstrap";
+import mqtt from 'mqtt';
+
 
 const ControlPanel = () => {
   const [lightToggle, setLightToggle] = useState(false);
   const [waterToggle, setWaterToggle] = useState(false);
 
+
+  const client = mqtt.connect("ws://192.168.10.79:8888");
+  console.log("client",client)
+
+  client.on('connect', () => console.log('publisher.connected.'));
+  let topic = "light/request"
+
+ const toggleButton = ()=>{
+  if(!lightToggle){
+    setLightToggle(true) 
+    client.publish(topic, "/////////////////////turn on//////////////");
+    console.log("[ON]/////////////////////turn on//////////////")
+  } else{
+    setLightToggle(false)
+    client.publish(topic, "/////////////////////turn off//////////////");
+    console.log("[OFF]/////////////////////turn off//////////////")
+  } 
+ }
+
   return (
     <div className="font-mono  w-full  bg-apple bg-apple-200 shadow-gray-400 shadow-lg">
+      {/* {messages.map((message,key) => (
+        <h2 key={key}>{message}</h2>
+     ))} */}
       <div className="flex flex-col  mt-14 mb-10">
         <div className="w-8/12 ml-52 bg-apple-100 shadow-lg flex flex-row justify-center gap-10 shadow-gray-600 rounded-lg p-3">
           <h2 className=" text-2xl select-none">Light</h2>
           <input
             type="checkbox"
             className="toggle toggle-lg toggle-accent"
-            onChange={() =>
-              !lightToggle ? setLightToggle(true) : setLightToggle(false)
-            }
+            onChange={toggleButton}
           />
         </div>
         <div className="w-8/12 ml-52 mt-14 bg-apple-100 shadow-lg flex flex-row justify-center gap-10 shadow-gray-600 rounded-lg p-3">
