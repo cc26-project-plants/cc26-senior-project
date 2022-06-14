22ace9a6c833b771564312605e1a38286b93ee24
lzs95 <86816779+lzs95@users.noreply.github.com>
2022-06-14 16:29:55 +0900

    feat: env file addition properly

--- context/MqttContext.tsx
+++ context/MqttContext.tsx
@@ -1,16 +1,10 @@
 import React, { useContext, useState, useEffect, createContext } from "react";
 import * as mqtt from "mqtt";
 
-// let client: mqtt.MqttClient = mqtt.connect(
-//   "wss://hairdresser.cloudmqtt.com:36484"
-// );
-// let client: mqtt.MqttClient = mqtt.connect("wss://192.168.10.79:8808");
-// let client: mqtt.MqttClient = mqtt.connect("wss://happa-26-mqtt.an.r.appspot.com:8808");
-
 const MqttContext = createContext<any>({});
-var client = mqtt.connect(`wss://hairdresser.cloudmqtt.com:36484`, {
-  username: `zabazjfs`,
-  password: `YgpLm5xPCxBY`,
+var client = mqtt.connect(`${process.env.CLOUD_MQTT_URL}`, {
+  username: process.env.CLOUD_MQTT_USERNAME,
+  password: process.env.CLOUD_MQTT_PASSWORD,
 });
 
 export function useMqtt() {
@@ -34,6 +28,7 @@ export default function MqttProvider({
   let topicWater: string = `water/${plantID}/request`;
 
   useEffect(() => {
+    console.log(process.env.CLOUD_MQTT_URL);
     client.on("connect", () => setConnectionStatus(true));
     client.publish(topic, messages);
   }, [lightToggle]);
